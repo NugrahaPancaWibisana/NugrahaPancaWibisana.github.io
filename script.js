004431f689acc6729ba1e8aa78b8b6016b40faca
NugrahaPancaWibisana <nugrahapancawibisana@gmail.com>
2023-07-01 19:25:51 +0700

    Update

---
+++ script.js
@@ -0,0 +1,7 @@
+const observer1 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        entry.target.classList.toggle("show", entry.isIntersecting);
+    });
+});
+const hiddenElements1 = document.querySelectorAll(".hidden");
+hiddenElements1.forEach((el) => observer1.observe(el));
