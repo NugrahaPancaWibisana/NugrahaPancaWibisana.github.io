c4446c1e8ee9f136a4ad08095405e231ccd60922
NugrahaPancaWibisana <nugrahapancawibisana@gmail.com>
2023-07-02 19:28:57 +0700

    Update

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ const observer1 = new IntersectionObserver((entries) => {
 });
 const hiddenElements1 = document.querySelectorAll(".hidden");
 hiddenElements1.forEach((el) => observer1.observe(el));
+
+function hamburgerMenu(x) {
+    x.classList.toggle("change");
+}
