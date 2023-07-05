6ee9741759bbe4068c8997120f7723a88b14e6d8
NugrahaPancaWibisana <nugrahapancawibisana@gmail.com>
2023-07-05 21:34:05 +0700

    update

--- js/script.js
+++ js/script.js
@@ -25,34 +25,34 @@ function ubahGambarSecaraBergantian() {
   var img = document.getElementById("myImage");
   var currentImageIndex = 1;
   var totalImages = 4;
-  
-  setInterval(function() {
+
+  setInterval(function () {
     currentImageIndex = (currentImageIndex % totalImages) + 1;
     img.src = "assets/pompom-" + currentImageIndex + ".png";
   }, 2000);
 }
-  
+
 ubahGambarSecaraBergantian();
 
-const buttons = document.querySelectorAll('.qna');
-const descriptions = document.querySelectorAll('.answer');
-const icons = document.querySelectorAll(".icon-about")
+const buttons = document.querySelectorAll(".qna");
+const descriptions = document.querySelectorAll(".answer");
+const icons = document.querySelectorAll(".icon-about");
 
 buttons.forEach((button, index) => {
-  button.addEventListener('click', () => {
-    if (descriptions[index].style.display === 'block') {
-      descriptions[index].style.display = 'none';
-      icons[index].style.transform = 'rotate(0deg)'; 
+  button.addEventListener("click", () => {
+    if (descriptions[index].style.display === "block") {
+      descriptions[index].style.display = "none";
+      icons[index].style.transform = "rotate(0deg)";
     } else {
       descriptions.forEach((description) => {
-        description.style.display = 'none';
+        description.style.display = "none";
       });
       icons.forEach((icon) => {
-        icon.style.transform = 'rotate(0deg)';
+        icon.style.transform = "rotate(0deg)";
       });
 
-      descriptions[index].style.display = 'block';
-      icons[index].style.transform = 'rotate(90deg)';
+      descriptions[index].style.display = "block";
+      icons[index].style.transform = "rotate(90deg)";
     }
   });
 });
